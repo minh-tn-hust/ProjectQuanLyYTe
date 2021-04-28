06fa8c26b0bd92057eb48a71ac61e583da7a276e
kimhunghk28102001 <hung.tk194294@sis.hust.edu.vn>
2021-04-28 20:29:02 +0700

    Sua CSDL

---
+++ YTeModel/NhanVienYTe.cs
@@ -0,0 +1,23 @@
+namespace YTeDB
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("NhanVienYTe")]
+    public partial class NhanVienYTe
+    {
+        [Key]
+        public int ID_NhanVien { get; set; }
+
+        public int? ID_Nguoi { get; set; }
+
+        [StringLength(50)]
+        public string UserName { get; set; }
+
+        [StringLength(50)]
+        public string Password { get; set; }
+    }
+}
