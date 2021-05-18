ba2b0e58e35a3d19bb0dc28d3b4017c5e60801dc
Minh Trần Nhật <minh.tn.hust@gmail.com>
2021-05-18 08:28:59 +0700

    R

--- LastModel/Migrations/202105171749056_init.Designer.cs
+++ LastModel/Migrations/202105171749056_init.Designer.cs
@@ -7,13 +7,13 @@ namespace LastModel.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.4.4")]
-    public sealed partial class Init : IMigrationMetadata
+    public sealed partial class init : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(Init));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(init));
         
         string IMigrationMetadata.Id
         {
-            get { return "202105160847028_Init"; }
+            get { return "202105171749056_init"; }
         }
         
         string IMigrationMetadata.Source
